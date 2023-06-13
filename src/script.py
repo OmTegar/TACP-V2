c37c90da3f32760e9236233ce40cb09993e14d6a
TEGAR_71105 <tegardito02@gmail.com>
2023-06-14 00:10:12 +0700

    update

--- src/script.py
+++ src/script.py
@@ -107,7 +107,7 @@ def web_static():
     print("   {99}-Back To The Main Menu \n\n")
     choice4 = input("TACP >> ")
     if choice4 == "1":
-        os.system("python3 /WebStatic/sektema/sektema.py")
+        os.system("python3 /src/WebStatic/sektema/sektema.py")
     if choice4 == "2":
         os.system("python3 /WebStatic/tegar/tegar.py")
     elif choice4 == "99":
