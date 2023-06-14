3d8bd79dc45ec1554fa27bc26fe128fa0f5c0c36
TEGAR_71105 <tegardito02@gmail.com>
2023-06-14 11:29:00 +0700

    update

--- src/testing.py
+++ src/testing.py
@@ -34,14 +34,14 @@ RESET = "\x1b[0m"
 
 def soon():
     print('''\033[91m
-                      
-    .dP"Y8  dP"Yb   dP"Yb  88b 88 
-    `Ybo." dP   Yb dP   Yb 88Yb88 
-    o.`Y8b Yb   dP Yb   dP 88 Y88 
-    8bodP'  YbodP   YbodP  88  Y8 
- 
+
+    .dP"Y8  dP"Yb   dP"Yb  88b 88
+    `Ybo." dP   Yb dP   Yb 88Yb88
+    o.`Y8b Yb   dP Yb   dP 88 Y88
+    8bodP'  YbodP   YbodP  88  Y8
+
     [!] I WILL ADD THIS TOOLS IN NEXT UPDATE [!]
-         
+
  \033[0m''')
     print("  {99}-Back To Main Menu \n\n")
     choice2 = raw_input("RAT >> ")
@@ -58,22 +58,22 @@ def soon():
 
 # Set banner text
 banner = GREEN + '''
-    8888888 8888888888   .8.           ,o888888o.    8 888888888o   
-          8 8888        .888.         8888      88.  8 8888     88. 
-          8 8888       :88888.     ,8 8888        8. 8 8888      88 
-          8 8888      .  88888.    88 8888           8 8888      88 
-          8 8888     .8.  88888.   88 8888           8 8888.   ,88  
-          8 8888    .8 8.  88888.  88 8888           8 888888888P'  
-          8 8888   .8'  8.  88888. 88 8888           8 8888         
-          8 8888  .8'    8.  88888. 8 8888       .8  8 8888         
-          8 8888 .888888888.  88888.  8888     ,88'  8 8888         
-          8 8888.8'        8.  88888.   8888888P'    8 8888    
+    8888888 8888888888   .8.           ,o888888o.    8 888888888o
+          8 8888        .888.         8888      88.  8 8888     88.
+          8 8888       :88888.     ,8 8888        8. 8 8888      88
+          8 8888      .  88888.    88 8888           8 8888      88
+          8 8888     .8.  88888.   88 8888           8 8888.   ,88
+          8 8888    .8 8.  88888.  88 8888           8 888888888P'
+          8 8888   .8'  8.  88888. 88 8888           8 8888
+          8 8888  .8'    8.  88888. 8 8888       .8  8 8888
+          8 8888 .888888888.  88888.  8888     ,88'  8 8888
+          8 8888.8'        8.  88888.   8888888P'    8 8888
 
-           ~ Package Global Scripting Linux Version 2.1 ~ 
+           ~ Package Global Scripting Linux Version 2.1 ~
 '''
 
 # Set credit text
-credit = BLUE + '''  
+credit = BLUE + '''
     [+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]
      [+]                                                             [+]
     [+]         ✔✔✔ Your application has been installed ✔✔✔         [+]
@@ -97,81 +97,6 @@ def error_message(message):
     print(f"{RED}[X] {message}{RESET}")
 
 
-def clearScr():
-    """
-    clear the screen in case of GNU/Linux or
-    windows
-    """
-    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-        os.system('clear')
-    elif sys.platform.startswith('win'):
-        os.system('cls')
-
-
-def update_tacp():
-    print("This Tool is Only Available for Linux and Similar Systems. ")
-    choiceupdate = input("Continue Y / N: ")
-    if choiceupdate in ['Y', 'y']:
-        os.system("git clone https://github.com/OmTegar/TACP-V2.git")
-        os.system("cd TACP-V2 && sudo bash ./src/update.sh")
-        os.system("tacp")
-
-
-def nginx_installed_check():
-    nginx_installed = subprocess.Popen(
-        ["dpkg", "-l", "nginx"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    nginx_output, _ = nginx_installed.communicate()
-
-    if "ii  nginx" in nginx_output.decode():
-        print("Nginx is installed, uninstalling and removing all files...")
-        subprocess.run(["systemctl", "stop", "nginx"])
-        subprocess.run(["apt-get", "remove", "--purge", "nginx",
-                        "nginx-common", "nginx-full", "-y"])
-        subprocess.run(["apt-get", "autoremove", "-y"])
-        subprocess.run(["rm", "-rf", "/etc/nginx"])
-        subprocess.run(["rm", "-rf", "/var/log/nginx"])
-        print("Nginx has been uninstalled and all files removed.")
-    else:
-        print("Nginx is not installed.")
-
-    apache_installed = subprocess.Popen(
-        ["which", "apache2"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    apache_output, _ = apache_installed.communicate()
-
-    if "apache2" not in apache_output.decode():
-        print("Installing Apache2...")
-        subprocess.run(["apt", "install", "apache2", "-y"])
-        print("Apache2 has been installed.")
-
-    subprocess.run(["service", "apache2", "start"])
-
-
-def apache_installed_check():
-    apache_installed = subprocess.Popen(
-        ["which", "apache2"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    apache_output, _ = apache_installed.communicate()
-
-    if "apache2" in apache_output.decode():
-        print("Apache2 is installed, uninstalling and removing all files...")
-        subprocess.run(["service", "apache2", "stop"])
-        subprocess.run(["apt-get", "remove", "--purge", "apache2", "-y"])
-        subprocess.run(["apt-get", "autoremove", "-y"])
-        subprocess.run(["rm", "-rf", "/etc/apache2"])
-        subprocess.run(["rm", "-rf", "/var/log/apache2"])
-        print("Apache2 has been uninstalled and all files removed.")
-    else:
-        print("Apache2 is not installed.")
-
-    nginx_installed = subprocess.Popen(
-        ["which", "nginx"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    nginx_output, _ = nginx_installed.communicate()
-
-    if "nginx" not in nginx_output.decode():
-        print("Installing Nginx...")
-        subprocess.run(["apt", "install", "nginx", "-y"])
-        print("Nginx has been installed.")
-
-    subprocess.run(["service", "nginx", "start"])
 
 def configure_nginx(port):
     nginx_conf = f'''
@@ -277,86 +202,6 @@ console.log("Server running at http://127.0.0.1:" + port);
     with open("/var/www/node-website-static1/index.js", "w") as index_js_file:
         index_js_file.write(index_js)
 
-def install_web_static(repository, path):
-    try:
-        if os.path.exists(path):
-            print("Removing existing application directory...")
-            subprocess.run(["rm", "-rf", path])
-            subprocess.run(["git", "clone", repository, path])
-            print(
-                "The installation process of the application has been successfully executed")
-        else:
-            subprocess.run(["git", "clone", repository, path])
-            print(
-                "The installation process of the application has been successfully executed")
-        subprocess.run(["chmod", "777", "-R", path])
-
-        config_text = f'''<VirtualHost *:80>
-        ServerAdmin webmaster@localhost
-        DocumentRoot {path}
-
-        ErrorLog ${{APACHE_LOG_DIR}}/error.log
-        CustomLog ${{APACHE_LOG_DIR}}/access.log combined
-        </VirtualHost>
-        '''
-
-        with open("/etc/apache2/sites-available/000-default.conf", "w") as config_file:
-            config_file.write(config_text)
-
-        subprocess.run(["service", "apache2", "restart"])
-
-        clearScr()
-        print(credit + """\033[1m
-             [!] Credit By OmTegar [!] https://omtegar.me [!]
-         """)
-        web_static()
-
-    except subprocess.CalledProcessError as e:
-        print("There was an error during the installation process of the application:")
-        print(e)
-
-def web_static():
-    print(banner + """\033[1m
-   [!] Some Tools By OmTegar WebServer - Static [!]
-  \033[0m""")
-    print("   {1}--Company Profile Sektema ")
-    print("   {2}--Company Profile OmTegar")
-    print("   {3}--Company Profile Aisyatul")
-    print("   {4}--Bootstrap Template Studio")
-    print("   {5}--Mini Games By OmTegar ( Basics )")
-    print("   {99}-Back To The Main Menu \n\n")
-    choice4 = input("TACP/WebStatic/ >> ")
-    if choice4 == "1":
-        nginx_installed_check()
-        install_web_static("https://github.com/OmTegar/company-profile-sektema.git",
-                           "/var/www/html/company-profile-sektema/")
-    elif choice4 == "2":
-        nginx_installed_check()
-        install_web_static("https://github.com/OmTegar/my-company-profile.git",
-                           "/var/www/html/my-company-profile/")
-    elif choice4 == "3":
-        nginx_installed_check()
-        install_web_static(
-            "https://github.com/OmTegar/company-aisyatul.git", "/var/www/html/company-aisyatul/")
-    elif choice4 == "4":
-        nginx_installed_check()
-        install_web_static("https://github.com/OmTegar/test-bootstrap-studio.git",
-                           "/var/www/html/test-bootstrap-studio/")
-    elif choice4 == "5":
-        nginx_installed_check()
-        install_web_static("https://github.com/OmTegar/mini-games-html-css-js-basic.git",
-                           "/var/www/html/mini-games-html-css-js-basic/")
-    elif choice4 == "99":
-        clearScr()
-        menu()
-    elif choice4 == "":
-        clearScr()
-        menu()
-    else:
-        clearScr()
-        menu()
-
-
 def install_framework_static_node(repository, path):
     try:
         if os.path.exists(path):
@@ -394,23 +239,15 @@ def install_framework_static_node(repository, path):
         print(e)
         subprocess.run(["pm2", "list"])
 
-def web_framework_static_react():
-    print("web_framework_static_react()")
-
-
-def web_framework_static_next():
-    print("web_framework_static_next()")
-
 def web_framework_static_node():
     print(banner + """\033[1m
    [!] Some Tools By OmTegar WebFramework - Static - node [!]
   \033[0m""")
-    print("   {1}--node tegar")
+    print("   {1}--node1")
     print("   {2}--node2")
     print("   {99}-Back To The Main Menu \n\n")
     choice4 = input("TACP >> ")
     if choice4 == "1":
-        apache_installed_check()
         install_framework_static_node("https://github.com/OmTegar/node-website-static1.git" , "/var/www/node-website-static1")
     elif choice4 == "2":
         soon()
@@ -424,7 +261,6 @@ def web_framework_static_node():
         clearScr()
         menu()
 
-
 def web_framework_static():
     print(banner + """\033[1m
    [!] Some Tools By OmTegar WebFramework - Static [!]
@@ -435,11 +271,12 @@ def web_framework_static():
     print("   {99}-Back To The Main Menu \n\n")
     choice4 = input("TACP/WebFramework >> ")
     if choice4 == "1":
-        web_framework_static()
+        apache_installed_check()
+        web_framework_static_node()
     elif choice4 == "2":
-        web_framework_static_react()
+        soon()
     elif choice4 == "3":
-        web_framework_static_next()
+        soon()
     elif choice4 == "99":
         clearScr()
         menu()
@@ -449,6 +286,7 @@ def web_framework_static():
     else:
         clearScr()
         menu()
+
 
 
 def web_Framework():
