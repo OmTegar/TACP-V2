2002112d4fb2bb0be63a703ecd404aff05ef4090
Tegar Dito Priandika <tegardito02@gmail.com>
2023-06-15 23:17:47 +0700

    Revert "Development FTP Server Fiture"

--- src/script.py
+++ src/script.py
@@ -102,7 +102,7 @@ banner = GREEN + '''
 credit = BLUE + '''  
     [+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]
      [+]                                                             [+]
-    [+]             ✔✔✔ Your application has been installed ✔✔✔     [+]
+    [+]         ✔✔✔ Your application has been installed ✔✔✔         [+]
      [+]                                                             [+]
     [+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]
 '''
@@ -133,7 +133,7 @@ def update_tacp():
     print("This Tool is Only Available for Linux and Similar Systems. ")
     choiceupdate = input("Continue Y / N: ")
     if choiceupdate in ['Y', 'y']:
-        os.system("git clone --single-branch --branch DEVELOPMENT https://github.com/OmTegar/TACP-V2.git")
+        os.system("git clone https://github.com/OmTegar/TACP-V2.git")
         os.system("cd TACP-V2 && sudo bash ./src/update.sh")
         os.system("tacp")
 
